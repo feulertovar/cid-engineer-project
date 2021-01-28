58f66fa8e0a80a415bea0278df2e5366aa2a2873
feuler <feuler@In-the-MacBook-Air.local>
2021-01-28 02:21:15 -0500

    added dateTime to index Response, test for EC2 HTTP

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Index response');
+  res.send('Index response at: ' + dateTime );
 });
 
 module.exports = router;
